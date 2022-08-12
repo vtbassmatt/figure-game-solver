786b85b794c712fa87e5ca7fde62e4d94b41b166
Matt Cooper <vtbassmatt@gmail.com>
2022-08-12 09:41:52 -0400

    click/unclick

--- figure_game_solver/__main__.py
+++ figure_game_solver/__main__.py
@@ -3,4 +3,9 @@ from .solver import Board
 if __name__ == '__main__':
     b = Board()
     b.print()
-    
+    print()
+    b.click(0)
+    b.print()
+    print()
+    b.unclick()
+    b.print()
--- figure_game_solver/solver.py
+++ figure_game_solver/solver.py
@@ -1,3 +1,4 @@
+from collections import ChainMap
 from enum import Enum, auto
 
 class Cell(Enum):
@@ -10,29 +11,45 @@ class Cell(Enum):
 class Board:
     def __init__(self):
         # puzzle 46
-        self.puzzle = (
-            # first column, top to bottom
+        self.base_puzzle = (
+            # first column, bottom to top
             (Cell.Magenta, Cell.Magenta, Cell.Yellow,  Cell.Magenta, Cell.Magenta),
-            (Cell.Magenta, Cell.White,   Cell.Cyan,    Cell.White,   Cell.Cyan),
-            (Cell.White,   Cell.White,   Cell.White,   Cell.Cyan,    Cell.Magenta),
-            (Cell.White,   Cell.Cyan,    Cell.Magenta, Cell.White,   Cell.Cyan),
-            # last column, top to bottom
-            (Cell.Cyan,    Cell.Cyan,    Cell.Magenta, Cell.White,   Cell.Magenta),
+            (Cell.Cyan,    Cell.White,   Cell.Cyan,    Cell.White,   Cell.Magenta),
+            (Cell.Magenta, Cell.Cyan,    Cell.White,   Cell.White,   Cell.White),
+            (Cell.Cyan,    Cell.White,   Cell.Magenta, Cell.Cyan,    Cell.White),
+            # last column, bottom to top
+            (Cell.Magenta, Cell.White,   Cell.Magenta, Cell.Cyan,    Cell.Cyan),
         )
+        self.puzzle = ChainMap(self.base_puzzle)
+    
+    def click(self, column):
+        "Click on a column to remove the bottom cell."
+        new_col = self.puzzle[column][1:]
+        self.puzzle = self.puzzle.new_child({column: new_col})
+    
+    def unclick(self):
+        "Remove the last click."
+        if len(self.puzzle.maps) > 1:
+            self.puzzle = self.puzzle.parents
+        else:
+            raise IndexError("There's nothing to unclick")
     
     def print(self):
-        cols = len(self.puzzle)
-        rows = len(self.puzzle[0])
-        for row in range(rows):
+        cols = len(self.base_puzzle)
+        rows = len(self.base_puzzle[0])
+        for row in range(rows - 1, -1, -1):
             for col in range(cols):
-                match self.puzzle[col][row]:
-                    case Cell.Magenta:
-                        char = '🔺'
-                    case Cell.Yellow:
-                        char = '🟡'
-                    case Cell.Cyan:
-                        char = '🔹'
-                    case Cell.White:
-                        char = '⬜️'
-                print(char, end='')
+                try:
+                    match self.puzzle[col][row]:
+                        case Cell.Magenta:
+                            char = '🔺'
+                        case Cell.Yellow:
+                            char = '🟡'
+                        case Cell.Cyan:
+                            char = '🔹'
+                        case Cell.White:
+                            char = '⬜️'
+                    print(char, end='')
+                except IndexError:
+                    print('  ', end='')
             print('')
